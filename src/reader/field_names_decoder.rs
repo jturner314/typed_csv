04403c5e9fbd75c3aa05304281b72f1f215961fb
Jim Turner <jturner314@gmail.com>
2017-02-04 03:13:14 -0500

    Remove unnecessary decoder logic for enums

--- src/reader/field_names_decoder.rs
+++ src/reader/field_names_decoder.rs
@@ -7,8 +7,6 @@ use rustc_serialize::Decoder;
 /// `rustc_serialize::Decodable`.
 #[derive(Debug)]
 pub struct FieldNamesDecoder {
-    stack: Vec<ByteString>,
-    popped: usize,
     field_names: Vec<ByteString>,
 }
 
@@ -17,53 +15,12 @@ impl FieldNamesDecoder {
     pub fn new(mut bytes: Vec<ByteString>) -> FieldNamesDecoder {
         bytes.reverse();
         FieldNamesDecoder {
-            stack: bytes,
-            popped: 0,
             field_names: Vec::new(),
         }
     }
 
     pub fn into_field_names(self) -> Vec<ByteString> {
         self.field_names
-    }
-
-    fn len(&self) -> usize {
-        self.stack.len()
-    }
-}
-
-impl FieldNamesDecoder {
-    fn pop(&mut self) -> Result<ByteString> {
-        self.popped += 1;
-        match self.stack.pop() {
-            None => {
-                self.err(format!("Expected a record with length at least {}, but got a record \
-                                  with length {}.",
-                                 self.popped,
-                                 self.popped - 1))
-            }
-            Some(bytes) => Ok(bytes),
-        }
-    }
-
-    fn pop_string(&mut self) -> Result<String> {
-        String::from_utf8(try!(self.pop())).map_err(|bytes| {
-            Error::Decode(format!("Could not convert bytes '{:?}' to UTF-8.", bytes))
-        })
-    }
-
-    fn push(&mut self, s: ByteString) {
-        self.stack.push(s);
-    }
-
-    fn push_string(&mut self, s: String) {
-        self.push(s.into_bytes());
-    }
-
-    fn err<'a, T, S>(&self, msg: S) -> Result<T>
-        where S: Into<String>
-    {
-        Err(Error::Decode(msg.into()))
     }
 }
 
@@ -144,20 +101,10 @@ impl Decoder for FieldNamesDecoder {
         f(self)
     }
 
-    fn read_enum_variant<T, F>(&mut self, names: &[&str], mut f: F) -> Result<T>
+    fn read_enum_variant<T, F>(&mut self, _: &[&str], mut f: F) -> Result<T>
         where F: FnMut(&mut Self, usize) -> Result<T>
     {
-        for i in 0..names.len() {
-            let cur = self.pop_string()?;
-            self.push_string(cur.clone());
-            match f(self, i) {
-                Ok(v) => return Ok(v),
-                Err(_) => {
-                    self.push_string(cur);
-                }
-            }
-        }
-        self.err(format!("Could not load value into any variant in {:?}", names))
+        f(self, 0)
     }
 
     fn read_enum_variant_arg<T, F>(&mut self, _: usize, f: F) -> Result<T>
@@ -223,29 +170,19 @@ impl Decoder for FieldNamesDecoder {
     fn read_option<T, F>(&mut self, mut f: F) -> Result<T>
         where F: FnMut(&mut Self, bool) -> Result<T>
     {
-        let s = try!(self.pop_string());
-        if s.is_empty() {
-            f(self, false)
-        } else {
-            self.push_string(s);
-            match f(self, true) {
-                Ok(v) => Ok(v),
-                Err(_) => f(self, false),
-            }
-        }
+        f(self, false)
     }
 
-    fn read_seq<T, F>(&mut self, f: F) -> Result<T>
+    fn read_seq<T, F>(&mut self, _: F) -> Result<T>
         where F: FnOnce(&mut Self, usize) -> Result<T>
     {
-        let len = self.len();
-        f(self, len)
+        unimplemented!();
     }
 
-    fn read_seq_elt<T, F>(&mut self, _: usize, f: F) -> Result<T>
+    fn read_seq_elt<T, F>(&mut self, _: usize, _: F) -> Result<T>
         where F: FnOnce(&mut Self) -> Result<T>
     {
-        f(self)
+        unimplemented!();
     }
 
     fn read_map<T, F>(&mut self, _: F) -> Result<T>
